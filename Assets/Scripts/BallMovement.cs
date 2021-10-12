741d497fe9d699b7cdc76e836f5a7720633e3aa5
WillRut17 <78185665+WillRut17@users.noreply.github.com>
2021-10-12 18:10:33 -0400

    Added Movement

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,30 +4,70 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    public float speed;
+    public float minSpeed;
+    public float turningSpeed;
+    public float maxSpeed;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
+        float horizontal = Input.GetAxis("Horizontal");
+        float forward = Input.GetAxis("Vertical");
 
-        }
-        if (Input.GetKey("s"))
-        {
+        Vector3 tempR = this.transform.localEulerAngles;
+        tempR.y += turningSpeed * Time.deltaTime * horizontal;
 
-        }
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-
+            if (speed < maxSpeed)
+            {
+                speed += 0.05f;
+                controller.Move(this.transform.forward * speed * forward * Time.deltaTime);
+                Debug.Log(speed);
+            }
+            else if (speed >= maxSpeed)
+            {
+                speed = maxSpeed;
+                controller.Move(this.transform.forward * speed * forward * Time.deltaTime);
+            }
         }
-        if (Input.GetKey("d"))
+        else if (!Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
         {
-
+            if (speed > minSpeed)
+            {
+                speed -= 0.05f;
+                controller.Move(this.transform.forward * speed * -forward * Time.deltaTime);
+                Debug.Log(speed);
+            }
+            else if (speed <= minSpeed)
+            {
+                speed = minSpeed;
+                controller.Move(this.transform.forward * speed * -forward * Time.deltaTime);
+            }
         }
+        else if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            if (speed > 0)
+            {
+                speed -= 0.05f;
+                controller.Move(this.transform.forward * speed * Time.deltaTime);
+                Debug.Log(speed);
+            }
+            else if (speed < 0)
+            {
+                speed += 0.05f;
+                controller.Move(this.transform.forward * speed * Time.deltaTime);
+                Debug.Log(speed);
+            }
+            else
+            {
+                speed = 0.0f;
+            }
+        }
+
+        this.transform.localEulerAngles = tempR;
     }
 }
